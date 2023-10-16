725635b1f0ce9d7e26892805d679f0de60de2e0e
GryphonClaw Software <gryphonclaw@gryphonclaw.net>
2023-10-16 12:47:55 -0700

    Modified the sample, game title, adde screenshot

--- app/main.rb
+++ app/main.rb
@@ -1,19 +1,20 @@
 require "app/url_label.rb"
 
 def tick args
+  #this just shows the DragonRuby version.
   args.outputs.labels << {
-    x: 5, y: 720, text: "DragonRuby Version: #{$gtk.version}", size_enum: -3
+    x: 5, y: 718, text: "DragonRuby Version: #{$gtk.version}", size_enum: -3
   }
 
-  args.state.url_label  ||= URLLabel.new(url: "https://www.gryphonclaw.net", text: "This is a test", title: "This is a title", size_enum: 0)
-  args.state.url_label2 ||= URLLabel.new(url: "https://www.gryphonclaw.net", text: "This is a test", title: "", size_enum: 5)
-  args.state.url_label3 ||= URLLabel.new(url: "https://www.gryphonclaw.net", text: "This is a test", title: "", size_enum: 8)
+  args.state.url_label  ||= URLLabel.new(url: "https://www.gryphonclaw.net", text: "GryphonClaw Website", title: "This is a title", size_enum: 0)
+  args.state.url_label2 ||= URLLabel.new(url: "https://www.dragonruby.org", text: "DragonRuby Website", title: "", size_enum: 5)
+  args.state.url_label3 ||= URLLabel.new(url: "https://www.gryphonclaw.net", text: "This label shows the url because of blank title", title: "", size_enum: 8)
 
+  #change some of the label settings
   my_label = args.state.url_label
   my_label.size_enum = 8
   my_label.x = 20
-  my_label.y = 120
-  # my_label.vertical_alignment_enum = 0
+  my_label.y = 100
 
   my_label = args.state.url_label2
   my_label.size_enum = 5
@@ -25,9 +26,12 @@ def tick args
   my_label.x = 20
   my_label.y = 300
 
+  #send processing to the labels
   args.state.url_label.tick(args)
   args.state.url_label2.tick(args)
   args.state.url_label3.tick(args)
+
+  #draw the labels
   args.outputs.labels << args.state.url_label
   args.outputs.labels << args.state.url_label2
   args.outputs.labels << args.state.url_label3
